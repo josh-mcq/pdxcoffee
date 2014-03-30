b7a6c35e855d8b6e4edd44e6044520109f895f7f
josh-mcq <joshua.g.mcquiston@gmail.com>
2014-03-30 15:59:50 -0700

    Create head.html.php

---
+++ head.html.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<!-- <link rel="stylesheet" type="text/css" href="css/coffeemain2.css"/>  -->
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	 <link href="css/pdxcoffee.css" rel="stylesheet">
+	 <link href="css/stripy_tables.css" rel="stylesheet">
+	 <script src="js/jquery-1.7.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<script src="js/core.js"></script>
+	  <script src="js/stripy_tables.js"></script>
+<title>PDX Coffee Shops</title>
+<style type= "text/css">
+ body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
+
+      @media (max-width: 980px) {
+        /* Enable use of floated navbar text */
+        .navbar-text.pull-right {
+          float: none;
+          padding-left: 5px;
+          padding-right: 5px;
+        }
+      }
+	  </style>
+</head>
