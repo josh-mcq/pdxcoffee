097053cba091fb51dadd6834ad95486721f0e8df
josh-mcq <joshua.g.mcquiston@gmail.com>
2014-03-30 15:57:59 -0700

    Create addshop.html.php

---
+++ addshop.html.php
@@ -0,0 +1,54 @@
+    <form action = "?" method = "post">
+    <input type = "hidden" id = "id" name = "id" Value = "id" />  <!--this should be type=hidden-->
+    <label for="coffeeshop">Name</label><input type = "text" id = "coffeeshop" name = "coffeeshop"/>
+    <label for= "location">Street Address</label><input type = "text" id = "location" name = "location"/>
+	<label for = "city">City, State</label><input type = "text" id = "city" name = "city" value = "Portland, OR"/>
+    <label for ="opentime">Open Time</label>
+	<select id= "opentime" name= "opentime">
+	  <option value="4:00am">4:00am</option>
+	  <option value="4:30am">4:30am</option>
+	  <option value="5:00am">5:00am</option>
+	  <option value="5:30am">5:30am</option>
+	  <option value="6:00am">6:00am</option>
+	  <option value="6:30am">6:30am</option>
+	  <option value="7:00am">7:00am</option>
+	  <option value="7:30am">7:30am</option>
+	  <option value="8:00am">8:00am</option>
+	  <option value="8:30am">8:30am</option>
+	  <option value="9:00am">9:00am</option>
+	  <option value="9:30am">9:30am</option>
+	</select>
+	  
+    <label for ="closetime">Open Time</label>
+	<select id= "closetime" name= "closetime">
+	  <option value="4:00pm">4:00pm</option>
+	  <option value="4:30pm">4:30pm</option>
+	  <option value="5:00pm">5:00pm</option>
+	  <option value="5:30pm">5:30pm</option>
+	  <option value="6:00pm">6:00pm</option>
+	  <option value="6:30pm">6:30pm</option>
+	  <option value="7:00pm">7:00pm</option>
+	  <option value="7:30pm">7:30pm</option>
+	  <option value="8:00pm">8:00pm</option>
+	  <option value="8:30pm">8:30pm</option>
+	  <option value="9:00pm">9:00pm</option>
+	  <option value="9:30pm">9:30pm</option>
+	  <option value="10:00pm">10:00pm</option>
+	  <option value="10:30pm">10:30pm</option>
+	  <option value="11:00pm">11:00pm</option>
+	  <option value="11:30pm">11:30pm</option>
+	  <option value="12:00am">12:00am</option>
+	  <option value="12:30am">12:30am</option>
+	  <option value="1:00am">1:00am</option>
+	  <option value="1:30am">1:30am</option>
+	  <option value="2:00am">2:00am</option>
+	  <option value="2:30am">2:30am</option>
+	  
+	</select>
+    <input type ="submit" value = "submit" />
+    </form>
+    <a href="?">Home</a>
+	
+	
+  </body>
+</html>
