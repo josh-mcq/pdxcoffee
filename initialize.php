f483c9e83c2c25d370eb55e424385169f03fe499
josh-mcq <joshua.g.mcquiston@gmail.com>
2014-03-30 16:02:50 -0700

    Create initialize.php

---
+++ initialize.php
@@ -0,0 +1,12 @@
+<?php
+// load config file first
+require_once("config.php");
+
+// load basic function next so that everything after can use them
+require_once("functions.php");
+
+// your connection file
+require_once("connection.php");
+
+
+?>
